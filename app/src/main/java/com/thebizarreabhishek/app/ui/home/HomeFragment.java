11fffece1c2ce20074ae9b1ff59b91dd098bce74
Abhishek Babu <abhishek@Abhisheks-MacBook-Air.local>
2025-12-06 19:17:07 +0530

    fix: Add missing SharedPreferences import in HomeFragment

--- app/src/main/java/com/thebizarreabhishek/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/thebizarreabhishek/app/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.thebizarreabhishek.app.ui.home;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.LayoutInflater;
